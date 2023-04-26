f1d16a9fb3de065cf11fd7839f38a28a6bacb636
maxmu <maxmu@KATANA>
2023-04-26 23:34:10 +0500

    Add project files.

---
+++ inheritence_csharp/Izlanuvchi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace inheritence_csharp
+{
+    class Izlanuvchi:Xodim
+    {
+        public string Fakultet { get; set; }
+        public string ilm_rah { get; set; }
+
+        public void FIR()
+        {
+            string s4 = string.Format("Fakultet:\t{0}\nIlmiy rahbari:\t{1}"
+                , Fakultet, ilm_rah);
+            Console.WriteLine(s4);
+        }
+    }
+    class Doktorant:Izlanuvchi
+    {
+        public int Kurs { get; set; }
+
+        public void kurs()
+        {
+            string s5 = string.Format("Kurs:\t{0}"
+                , Kurs);
+            Console.WriteLine(s5);
+        }
+    }
+}
